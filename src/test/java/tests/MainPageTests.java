bd5af4df3aa283dc5ec8a01e38e4246fd8e81b0b
Ivan Main PC <pola2288@yandex.ru>
2025-02-02 19:44:06 +0300

    connection Jenkins

---
+++ src/test/java/tests/MainPageTests.java
@@ -0,0 +1,170 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class MainPageTests extends TestConfiguration {
+
+    @Test
+    @DisplayName("Проверка работы смены языка с русского на английский.")
+    @Tag("smoke")
+    void LanguageChangeTest() {
+        step("Открываем главную страницу ", () -> {
+            open("");
+        });
+        step("Кликаем на меню с выбором языка ", () -> {
+            $(".lang_switcher").click();
+        });
+        step("Выбираем английский язык ", () -> {
+            $$(".lang_switcher ul.lang_selector li .lang-name").get(1).click();
+        });
+        step("Проверяем, что язык поменялся на английский ", () -> {
+            $("ul.menu li a").shouldHave(text("News"));
+        });
+        step("Кликаем на меню с выбором языка ", () -> {
+            $(".lang_switcher").click();
+        });
+        step("Возвращаем русский язык ", () -> {
+            $$(".lang_switcher ul.lang_selector li .lang-name").get(0).click();
+        });
+        step("Проверяем, что язык поменялся на русский ", () -> {
+            $("ul.menu li a").shouldHave(text("Новости"));
+        });
+    }
+
+    @Test
+    @DisplayName("Нажатие кнопок навигационного меню в правой части экрана, проверка текста в них.")
+    @Tag("smoke")
+    void rightNavigationMenuButtonClickCheckTest() {
+        step("Открываем главную страницу ", () -> {
+            open("");
+        });
+        step("Находим первый элемент в меню, проверяем текст в нем и кликаем на него ", () -> {
+            $$("ul.banner_nav li").get(0).hover().shouldHave(text("Приобрести")).click();
+        });
+        step("Находим второй элемент в меню, проверяем текст в нем и кликаем на него ", () -> {
+            $$("ul.banner_nav li").get(1).hover().shouldHave(text("Расширения")).click();
+        });
+        step("Находим третий элемент в меню, проверяем текст в нем и кликаем на него ", () -> {
+            $$("ul.banner_nav li").get(2).hover().shouldHave(text("patch 0.16")).click();
+        });
+        step("Находим четвертый элемент в меню, проверяем текст в нем и кликаем на него ", () -> {
+            $$("ul.banner_nav li").get(3).hover().shouldHave(text("Streets of Tarkov teaser #3")).click();
+        });
+        step("Находим пятый элемент в меню, проверяем текст в нем и кликаем на него ", () -> {
+            $$("ul.banner_nav li").get(4).hover().shouldHave(text("RAID")).click();
+        });
+        step("Находим шестой элемент в меню, проверяем текст в нем и кликаем на него ", () -> {
+            $$("ul.banner_nav li").get(5).hover().shouldHave(text("Улицы Таркова - тизер")).click();
+        });
+        step("Находим седьмой элемент в меню, проверяем текст в нем и кликаем на него ", () -> {
+            $$("ul.banner_nav li").get(6).hover().shouldHave(text("Official Announcement Trailer")).click();
+        });
+    }
+
+    @Test
+    @DisplayName("Запуск видео на главном экране")
+    @Tag("smoke")
+    void playbackVideoOnMainPageTest() {
+        step("Открываем главную страницу ", () -> {
+            open("");
+        });
+        step("Ищем первое видео и запускаем его ", () -> {
+            $("section#banner_42_youtube").click();
+        });
+        step("Ищем второе видео и запускаем его ", () -> {
+            $("section#banner_39_youtube").click();
+        });
+        step("Ищем третье видео и запускаем его ", () -> {
+            $("section#banner_38_youtube").click();
+        });
+        step("Ищем четвертое видео и запускаем его ", () -> {
+            $("section#banner_36_youtube").click();
+        });
+        step("Ищем пятое видео и запускаем его ", () -> {
+            $("section#banner_4_youtube").click();
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка заголовка и подзаголовка во вкладке новости и во вкладке патчи")
+    @Tag("smoke")
+    void checkingHeadlineAndMainTextNewsAndPatchTest() {
+        step("Открываем главную страницу ", () -> {
+            open("");
+        });
+        step("Находим кнопку 'Новости' в меню и нажимаем на нее ", () -> {
+            $("ul.menu li a").shouldHave(text("Новости")).click();
+        });
+        step("Проверяем наличие заголовка над новостями ", () -> {
+            $("section#news h2").shouldHave(text("Новости"));
+        });
+        step("Проверяем наличие заголовка и подзаголовка первой новости ", () -> {
+            $$("ul#news-list li .description").get(0).shouldHave(text("Представляем вашему вниманию результаты опроса по Барахолке."));
+            $$("ul#news-list li .description").get(0).shouldHave(text("Представляем вашему вниманию результаты опроса по Барахолке."));
+        });
+        step("Проверяем наличие заголовка и подзаголовка второй новости ", () -> {
+            $$("ul#news-list li .headtext a").get(1).shouldHave(text("Twitch Drops за подписки на стримеров!"));
+            $$("ul#news-list li .description").get(1).shouldHave(text("С 27 января 16:00 МСК по 3 февраля 16:00 МСК игроки могут получить уникальную Twitch Drops награду за подписку на стримеров на платформе."));
+        });
+        step("Проверяем наличие заголовка и подзаголовка третьей новости ", () -> {
+            $$("ul#news-list li .headtext a").get(2).shouldHave(text("Конкурс «Косплей на персонажа Escape from Tarkov и Escape from Tarkov: Arena»"));
+            $$("ul#news-list li .description").get(2).shouldHave(text("Приглашаем принять участие в конкурсе «Косплей на персонажа Escape from Tarkov и Escape from Tarkov: Arena»!"));
+        });
+        step("Переключаемся на таб 'Патчноуты' ", () -> {
+            $$(".news_filter .inline").get(1).shouldHave(text("Патчноуты")).click();
+        });
+        step("Проверяем наличие заголовка и подзаголовка первой новости в табе патчей ", () -> {
+            $$("ul#news-list li .headtext a").get(0).shouldHave(text("Патч 0.16.0.0"));
+            $$("ul#news-list li .description").get(0).shouldHave(text("Список изменений патча 0.16.0.0 для Escape from Tarkov."));
+        });
+        step("Проверяем наличие заголовка и подзаголовка второй новости в табе патчей ", () -> {
+            $$("ul#news-list li .headtext a").get(1).shouldHave(text("Патч 0.15.5.1"));
+            $$("ul#news-list li .description").get(1).shouldHave(text("Список изменений патча 0.15.5.1 для Escape from Tarkov."));
+        });
+        step("Проверяем наличие заголовка и подзаголовка третьей новости в табе патчей ", () -> {
+            $$("ul#news-list li .headtext a").get(2).shouldHave(text("Патч 0.15.5.0"));
+            $$("ul#news-list li .description").get(2).shouldHave(text("Список изменений патча 0.15.5.0 для Escape from Tarkov."));
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка работоспособности кнопок для управления новостями")
+    @Tag("smoke")
+    void checkFunctionalityButtonsUnderTheNewsTest() {
+        step("Открываем главную страницу ", () -> {
+            open("");
+        });
+        step("Переходим к новостям ", () -> {
+            $("ul.menu li a").shouldHave(text("Новости")).click();
+        });
+        step("Переходим на следующею новость по кнопке ", () -> {
+            $(".pager .pager-link.pager-next").click();
+            sleep(100);
+        });
+        step("Переходим на последнюю страницу новостей ", () -> {
+            $(".pager .pager-link.pager-last").click();
+        });
+        step("Переходим на первую страницу новостей ", () -> {
+            $(".pager .pager-link.pager-first").click();
+        });
+        step("Переключаемся на разные станицы новостей ", () -> {
+            $$(".pager .pager-link").get(1).click();
+            $$(".pager .pager-link").get(2).click();
+            $$(".pager .pager-link").get(3).click();
+            $$(".pager .pager-link").get(4).click();
+            $$(".pager .pager-link").get(0).click();
+        });
+        step("Жмем на кнопку 'Показать еще' ", () -> {
+            $("#show_more_news.button").click();
+        });
+        step("Жмем на кнопку 'Показать все' ", () -> {
+            $(".show_all .button").click();
+        });
+    }
+}
---
+++ src/test/java/tests/TestConfiguration.java
@@ -0,0 +1,50 @@
+package tests;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+
+public class TestConfiguration {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://www.escapefromtarkov.com";
+        Configuration.browser = System.getProperty("browserType");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.remote = System.getProperty("remoteHost");
+        Configuration.pageLoadStrategy = "eager";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void setupTest() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void teardown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+        Selenide.closeWebDriver();
+    }
+}
